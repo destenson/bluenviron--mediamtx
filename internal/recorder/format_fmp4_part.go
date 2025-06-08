b876c43fdcac4623de8ff613192b99bcccace773
Dennis E <git@destenson.digital>
2025-06-08 00:59:48 -0700

    Revert "attempt to make recorded files of exact size"
    This reverts commit 0c5affe6e5458510479075397931bd3318da818b.

--- internal/recorder/format_fmp4_part.go
+++ internal/recorder/format_fmp4_part.go
@@ -45,16 +45,12 @@ type formatFMP4Part struct {
 	sequenceNumber uint32
 	startDTS       time.Duration
 
-	partTracks       map[*formatFMP4Track]*fmp4.PartTrack
-	trackFrameCounts map[*formatFMP4Track]int
-	frameRates       map[*formatFMP4Track]float64
-	endDTS           time.Duration
+	partTracks map[*formatFMP4Track]*fmp4.PartTrack
+	endDTS     time.Duration
 }
 
 func (p *formatFMP4Part) initialize() {
 	p.partTracks = make(map[*formatFMP4Track]*fmp4.PartTrack)
-	p.trackFrameCounts = make(map[*formatFMP4Track]int)
-	p.frameRates = make(map[*formatFMP4Track]float64)
 }
 
 func (p *formatFMP4Part) close() error {
@@ -95,20 +91,9 @@ func (p *formatFMP4Part) write(track *formatFMP4Track, sample *sample, dts time.
 				int64(track.initTrack.TimeScale), int64(time.Second))),
 		}
 		p.partTracks[track] = partTrack
-		p.trackFrameCounts[track] = 0
-
-		// Calculate and store frame rate based on timescale and sample duration if possible
-		// This is an estimate and will be refined as more samples arrive
-		if sample.Duration > 0 {
-			p.frameRates[track] = float64(track.initTrack.TimeScale) / float64(sample.Duration)
-		}
-	} else if p.frameRates[track] == 0 && sample.Duration > 0 {
-		// Update frame rate if we have a valid sample duration
-		p.frameRates[track] = float64(track.initTrack.TimeScale) / float64(sample.Duration)
 	}
 
 	partTrack.Samples = append(partTrack.Samples, sample.Sample)
-	p.trackFrameCounts[track]++
 
 	endDTS := dts + timestampToDuration(int64(sample.Duration), int(track.initTrack.TimeScale))
 	if endDTS > p.endDTS {
@@ -120,21 +105,4 @@ func (p *formatFMP4Part) write(track *formatFMP4Track, sample *sample, dts time.
 
 func (p *formatFMP4Part) duration() time.Duration {
 	return p.endDTS - p.startDTS
-}
-
-// calculateExpectedFrameCount returns the expected number of frames
-// for a given track based on frame rate and part duration
-func (p *formatFMP4Part) calculateExpectedFrameCount(track *formatFMP4Track, partDuration time.Duration) int {
-	frameRate, ok := p.frameRates[track]
-	if !ok || frameRate <= 0 {
-		// Default to 30fps if we don't know the frame rate
-		frameRate = 30.0
-	}
-
-	expectedFrames := int(frameRate * partDuration.Seconds())
-	if expectedFrames < 1 {
-		expectedFrames = 1 // Ensure at least one frame
-	}
-
-	return expectedFrames
 }
